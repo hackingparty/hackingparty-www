078b91812d29aebb8913739965f4ce1ae86399e9
Glenn Y. Rolland <glenux@gmail.com>
2011-03-10 22:50:16 +0100

    hackingparty: Added quite complete api file (without urls, but still..)

---
+++ hackingparty/api.rb
@@ -0,0 +1,60 @@
+
+
+require 'raklur/database'
+
+require 'rubygems'
+require 'sinatra/base'
+require 'sass'
+
+
+#
+#
+#
+module HackingParty
+
+	#
+	#
+	#
+	class Api < Sinatra::Base
+
+		enable :sessions
+
+		# basic configuration
+		set :default_layout, :layout
+		set :static, true
+		set :public, File.expand_path( File.dirname(__FILE__) + '/../static' )
+		set :views, File.expand_path( File.dirname(__FILE__) + '/../views' )
+
+		# for web fonts
+		mime_type :ttf, "application/octet-stream"
+		mime_type :eot, "application/octet-stream"
+		mime_type :otf, "application/octet-stream"
+
+		#
+		# at first time
+		#
+		configure do
+			#DataMapper::Logger.new($stdout, :debug)
+			#DataMapper.setup(:default, 'sqlite:project2.db')
+			#DataMapper.auto_upgrade!
+		end
+
+		#
+		# CSS
+		#
+		get '/style.css' do
+			content_type 'text/css', :charset => 'utf-8'
+			scss :style
+		end
+
+		#
+		# Index URL
+		#
+		get '/' do
+			@body = "Hello world"
+			erb :index
+		end
+
+	end
+end
+
