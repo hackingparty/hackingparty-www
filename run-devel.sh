8facab68554970a1aec7dd6199d35731d0cee04f
Glenn Y. Rolland <glenux@gmail.com>
2011-03-10 23:29:21 +0100

    Added reference to path inside run-* scripts.

--- run-devel.sh
+++ run-devel.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+export PATH=/var/lib/gems/1.8/bin/:$HOME/.gem/ruby/1.8/bin
 shotgun config.ru -o 0.0.0.0 -p 9393
--- run-prod.sh
+++ run-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
+export PATH=/var/lib/gems/1.8/bin/:$HOME/.gem/ruby/1.8/bin
 rackup config.ru -o 0.0.0.0 -p 9393
 #thin -C thin-prod.yml -R config.ru start
